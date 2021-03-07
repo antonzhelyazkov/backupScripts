46938c8340709a7b08c06ce29789c83661621543
antonzhelyazkov <antonisimo@gmail.com>
2021-03-07 12:20:40 +0200

    bg_backup 2

--- pg_backup.py
+++ pg_backup.py
@@ -112,7 +112,7 @@ if not backup_dir['status']:
 
 current_date = datetime.datetime.now()
 print(current_date)
-current_date_string = current_date.strftime("%Y%m%d%H%M")
+current_date_string = current_date.strftime("%Y%m%d%H%M%s")
 print(current_date_string)
 
 if process_nagios_file(NAGIOS_FILE):
