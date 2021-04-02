06af22e8bf09da08a3a72091a6cf3ca043b4f2ec
antonzhelyazkov <antonisimo@gmail.com>
2021-04-02 21:45:43 +0300

    reoback

--- qaz.py
+++ qaz.py
@@ -28,8 +28,9 @@ def remove_excludes(file_list: list, excludes_list: list) -> list:
     filtered_files = []
     for item_all in file_list:
         for item_exclude in excludes_list:
-            print(item_exclude)
             if item_exclude in item_all:
+                pass
+            else:
                 filtered_files.append(item_all)
 
     return filtered_files
