535ab3aae2cee13e129fcf9c4757e4a1158a1436
antonzhelyazkov <antonisimo@gmail.com>
2021-04-02 23:21:05 +0300

    reoback

--- qaz.py
+++ qaz.py
@@ -26,6 +26,7 @@ def walk_files(directory: str) -> list:
 def remove_excludes(file_list: list, excludes_list: list) -> list:
     for element in file_list:
         for exclude in excludes_list:
+            print(element, exclude)
             if exclude in element:
                 print("rem", element)
                 file_list.remove(element)
