243a4995cf80876c7f39108c8264fd1123cd3637
antonzhelyazkov <antonisimo@gmail.com>
2021-04-03 21:14:55 +0300

    reoback

--- reoback.py
+++ reoback.py
@@ -138,12 +138,10 @@ if HOSTNAME is None or HOSTNAME == '':
 
 for item_arch in CONFIG_DATA['backup']:
     files_arr = walk_files(item_arch['path'])
-    for file in files_arr:
-        print(file)
-    # with tarfile.open(f"{item_arch['name']}.tar.gz", 'w:gz') as archive:
-    #     for file in files_arr:
-    #         archive.add(file, filter=excl)
-    #     archive.list()
+    with tarfile.open(f"{item_arch['name']}.tar.gz", 'w:gz') as archive:
+        for file in files_arr:
+            archive.add(file)
+        archive.list()
 
 if process_nagios_file(NAGIOS_FILE):
     os.remove(PID_FILE)
