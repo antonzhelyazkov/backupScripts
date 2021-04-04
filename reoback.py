13c32dd07d4478d1b169ec52ec0f43ba054979d1
antonzhelyazkov <antonisimo@gmail.com>
2021-04-04 09:46:50 +0300

    reoback

--- reoback.py
+++ reoback.py
@@ -141,10 +141,12 @@ if HOSTNAME is None or HOSTNAME == '':
 for item_arch in CONFIG_DATA['backup']:
     OUT_FILE = f"{add_slash(CONFIG_DATA['tmp_dir'])}{item_arch['name']}.tar.gz"
     tar_cmd = tar_command(item_arch['path'], item_arch['excludes'], OUT_FILE)
-    out = subprocess.run(tar_cmd, stdout=subprocess.PIPE)
-    print(out.returncode)
-    print(out.stdout)
-    print(out.stderr)
+    run_tar = subprocess.run(tar_cmd, stdout=subprocess.PIPE)
+    if run_tar.returncode != 0:
+        print_log(VERBOSE, f"ERROR in {item_arch['path']}")
+        sys.exit(1)
+    else:
+        print_log(VERBOSE, f"INFO archive successful {OUT_FILE}")
 
 if process_nagios_file(NAGIOS_FILE):
     os.remove(PID_FILE)
