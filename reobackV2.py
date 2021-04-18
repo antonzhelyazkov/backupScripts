b2c4b8df8e0bfbacda2f9068a33913edf8276a86
antonzhelyazkov <antonisimo@gmail.com>
2021-04-18 21:41:36 +0300

    reoback

--- reobackV2.py
+++ reobackV2.py
@@ -1,9 +1,16 @@
 import argparse
+import ftplib
 import json
 import logging
 import os
+import re
+import socket
+import subprocess
 import sys
 import time
+
+
+HOSTNAME = socket.gethostname().split(".")[0]
 
 
 class PidFileExists(Exception):
@@ -20,6 +27,15 @@ def add_slash(directory):
     else:
         dir_return = directory
     return dir_return
+
+
+def remove_slash(directory):
+    if directory.endswith("/"):
+        size = len(directory)
+        dir_return = directory[:size - 1]
+        return dir_return
+    else:
+        return directory
 
 
 def process_pid_file(pid_f: str) -> bool:
@@ -43,6 +59,129 @@ def check_dirs_exist(dirs: list):
 
     if len(err_dirs) > 0:
         raise DirectoryMissing(err_dirs)
+
+
+def tar_command(arch_dir: str, excludes: list, out_file: str) -> list:
+    tar_arr = ["/usr/bin/tar", "-cf", out_file, "-I", "pigz"]
+    if len(excludes) > 0:
+        for item_exclude in excludes:
+            tar_arr.extend([f"--exclude={remove_slash(item_exclude)}"])
+
+    tar_arr.append(arch_dir)
+
+    return tar_arr
+
+
+def ftp_session(ftp_host: str, ftp_user: str, ftp_pass: str):
+    try:
+        session = ftplib.FTP(ftp_host, ftp_user, ftp_pass, timeout=3)
+        return session
+    except ftplib.Error as e:
+        raise ftplib.Error(e)
+    except socket.timeout:
+        raise socket.timeout()
+
+
+def ftp_dir_remove(session, path: str) -> bool:
+    for (name, facts) in session.mlsd(path=path):
+        if name in ['.', '..']:
+            continue
+        elif facts['type'] == 'file':
+            session.delete(f"{path}/{name}")
+        elif facts['type'] == 'dir':
+            ftp_dir_remove(session, f"{path}/{name}")
+
+    try:
+        session.rmd(path)
+        return True
+    except ftplib.Error as e:
+        return False
+
+
+def ftp_backup_rotate(session, remote_dir: str, days_rotate: int, backup_stamp: int) -> bool:
+    seconds_minus = days_rotate * 86400
+    stamp_before = backup_stamp - seconds_minus
+
+    dirs_arr = []
+    for (name, facts) in session.mlsd(path=remote_dir):
+        if name in ['.', '..']:
+            continue
+        elif facts['type'] == 'dir' and re.match("^\d{10}$", name):
+            dirs_arr.append(name)
+
+    for item in dirs_arr:
+        if int(item) < stamp_before:
+            dir_to_remove = f"{remote_dir}/{item}"
+            if not ftp_dir_remove(session, dir_to_remove):
+                return False
+
+    return True
+
+
+def create_ftp_dir(directory: str, session) -> bool:
+    try:
+        session.mkd(directory)
+        return True
+    except ftplib.error_perm as perm:
+        return True
+    except ftplib.Error as e:
+        return False
+
+
+def ftp_upload(file: str, remote_dir: str, backup_stamp: int, session) -> bool:
+    f_name = os.path.basename(file)
+    dir_stamp = f"{remote_dir}/{backup_stamp}"
+
+    if not create_ftp_dir(remote_dir, session):
+        sys.exit(1)
+
+    if not create_ftp_dir(dir_stamp, session):
+        sys.exit(1)
+
+    file_fh = open(file, "rb")
+    try:
+        session.storbinary(f"STOR {dir_stamp}/{f_name}", file_fh)
+        return True
+    except ftplib.Error as e:
+        return False
+    finally:
+        file_fh.close()
+        session.close()
+
+
+def remove_local_dir(directory: str) -> bool:
+    files_arr = os.listdir(directory)
+    for item in files_arr:
+        file_to_remove = f"{directory}/{item}"
+        try:
+            os.remove(file_to_remove)
+        except OSError as e:
+            return False
+
+    try:
+        os.rmdir(directory)
+        return True
+    except OSError as e:
+        return False
+
+
+def remove_local_backups(days_rotate: int, backup_dir: str, backup_stamp: int) -> bool:
+    seconds_minus = days_rotate * 86400
+    stamp_before = backup_stamp - seconds_minus
+
+    dirs_arr = os.listdir(backup_dir)
+    dirs_to_process = []
+    for item in dirs_arr:
+        if re.match("^\d{10}$", item):
+            dirs_to_process.append(item)
+
+    for item in dirs_to_process:
+        if int(item) < stamp_before:
+            dir_to_remove = f"{backup_dir}{item}"
+            if not remove_local_dir(dir_to_remove):
+                return False
+
+    return True
 
 
 def main():
@@ -91,6 +230,52 @@ def main():
         check_dirs_exist(dirs_exists)
     except DirectoryMissing as e:
         logger.exception(f"#### {e}")
+
+    if HOSTNAME is None or HOSTNAME == '':
+        logger.info(f"ERROR in hostname {HOSTNAME}")
+        sys.exit(1)
+
+    backup_stamp = int(time.time())
+    backup_dir = f"{add_slash(config_data['tmp_dir'])}{str(backup_stamp)}/"
+    backup_ftp_dir = f"{HOSTNAME}-reoback"
+
+    for item_arch in config_data['backup']:
+        out_file = f"{backup_dir}{item_arch['name']}.tar.gz"
+        try:
+            os.makedirs(backup_dir, exist_ok=True)
+        except OSError as e:
+            logger.exception(f"#### {e}")
+            sys.exit(1)
+        tar_cmd = tar_command(item_arch['path'], item_arch['excludes'], out_file)
+        run_tar = subprocess.run(tar_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if run_tar.returncode != 0:
+            logger.info(f"ERROR in {item_arch['path']}")
+            logger.info(f"ERROR in {run_tar.stderr}")
+            sys.exit(1)
+        else:
+            logger.info(f"INFO archive successful {out_file}")
+            ftp_upload(out_file,
+                       backup_ftp_dir,
+                       backup_stamp,
+                       ftp_session(config_data['ftp_login']['ftp_host'],
+                                   config_data['ftp_login']['ftp_user'],
+                                   config_data['ftp_login']['ftp_pass']))
+
+    if ftp_backup_rotate(ftp_session(config_data['ftp_login']['ftp_host'],
+                                     config_data['ftp_login']['ftp_user'],
+                                     config_data['ftp_login']['ftp_pass']),
+                         backup_ftp_dir,
+                         config_data['ftp_backup_rotate'],
+                         backup_stamp):
+        logger.info(f"INFO all backups older than {config_data['ftp_backup_rotate']} are removed")
+    else:
+        logger.info(f"ERROR in remove FTP older backups")
+        sys.exit(1)
+
+    if not remove_local_backups(config_data['local_backup_rotate'],
+                                add_slash(config_data['tmp_dir']),
+                                backup_stamp):
+        sys.exit(1)
 
     try:
         f = open(nagios_file, "w")
