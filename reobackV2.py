4ed02d3f21dbd138c67a7f260a604935e878996e
antonzhelyazkov <antonisimo@gmail.com>
2021-04-18 20:05:17 +0300

    reoback

--- reobackV2.py
+++ reobackV2.py
@@ -3,6 +3,11 @@ import json
 import logging
 import os
 import sys
+import time
+
+
+class PidFileExists(Exception):
+    pass
 
 
 def add_slash(directory):
@@ -13,21 +18,31 @@ def add_slash(directory):
     return dir_return
 
 
+def process_pid_file(pid_f: str) -> bool:
+    if os.path.isfile(pid_f):
+        raise PidFileExists
+    else:
+        try:
+            f = open(pid_f, "w")
+            f.write(str(os.getpid()))
+            f.close()
+            return True
+        except IOError as e:
+            raise IOError(e)
+
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', '--config', type=str, required=True, help="Path to config file", dest='config')
-    parser.add_argument('-v', '--verbose', required=False, action='store_true', dest='verbose')
 
     args_cmd = parser.parse_args()
     config_file = args_cmd.config
-    verbose = args_cmd.verbose
 
     config_open = open(config_file, encoding='utf-8')
     config_data = json.load(config_open)
-    log_file_name = os.path.basename(sys.argv[0]).split(".")[0]
-    log_file = f"{add_slash(config_data['log_dir'])}{log_file_name}.log"
-    print(log_file)
+    script_name = os.path.basename(sys.argv[0]).split(".")
+    log_file = f"{add_slash(config_data['log_dir'])}{script_name[0]}.log"
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
@@ -35,17 +50,30 @@ def main():
     formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
     file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.ERROR)
+    file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    if verbose:
-        logger.addHandler(stream_handler)
 
-    logger.error("qweqwe")
+    pid_file = f"{add_slash(config_data['pid_file_path'])}{script_name}.pid"
+    nagios_file = f"{add_slash(config_data['pid_file_path'])}{script_name}.nagios"
+    try:
+        process_pid_file(pid_file)
+    except PidFileExists:
+        logger.info(f"INFO pid file {pid_file} exists")
+        sys.exit(1)
+    except OSError as e:
+        logger.exception(f"#### {e}")
+
+    try:
+        f = open(nagios_file, "w")
+        f.write(str(int(time.time())))
+        f.close()
+    except IOError as e:
+        logger.exception(f"##### {e}")
 
 
 if __name__ == "__main__":
