06d47b1e85eaf293bc8e247495e586e38ce5aaf6
antonzhelyazkov <antonisimo@gmail.com>
2021-05-01 10:52:19 +0300

    userside

---
+++ userside_backup.py
@@ -0,0 +1,24 @@
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename=f'{orig_func.__name__}', level=logging.INFO)
+
+    def wrapper(*args, **kwargs):
+        logging.info(f'Ran with args: {args}, and kwargs: {kwargs}')
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def main():
+    import time
+
+    def display_info(name, age):
+        time.sleep(1)
+        print('display_info ran with arguments ({}, {})'.format(name, age))
+
+    display_info("qweqwe", 123)
+
+
+if __name__ == "__main__":
+    main()
