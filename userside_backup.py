1d69efb8057a0c4d93de19c396550267d40c11bd
antonzhelyazkov <antonisimo@gmail.com>
2021-05-05 16:27:51 +0300

    userside

--- userside_backup.py
+++ userside_backup.py
@@ -2,7 +2,15 @@ import argparse
 import json
 import logging
 import os
+import socket
 import sys
+import time
+
+HOSTNAME = socket.gethostname().split(".")[0]
+
+
+class PidFileExists(Exception):
+    pass
 
 
 def add_slash(directory):
@@ -13,8 +21,17 @@ def add_slash(directory):
     return dir_return
 
 
-def test_log(log_function):
-    log_function.info("asdasd")
+def process_pid_file(pid_f: str) -> bool:
+    if os.path.isfile(pid_f):
+        raise PidFileExists
+    else:
+        try:
+            f = open(pid_f, "w")
+            f.write(str(os.getpid()))
+            f.close()
+            return True
+        except IOError as e:
+            raise IOError(e)
 
 
 def main():
@@ -50,8 +67,31 @@ def main():
     else:
         logger.addHandler(file_handler)
 
-    # logger.info("qweqwe")
-    test_log(logger)
+    pid_file = f"{add_slash(config_data['pid_file_path'])}{script_name[0]}.pid"
+    nagios_file = f"{add_slash(config_data['pid_file_path'])}{script_name[0]}.nagios"
+    try:
+        process_pid_file(pid_file)
+    except PidFileExists:
+        logger.info(f"INFO pid file {pid_file} exists")
+        sys.exit(1)
+    except OSError as e:
+        logger.exception(f"#### {e}")
+
+    backup_stamp = int(time.time())
+    backup_dir = f"{add_slash(config_data['tmp_dir'])}{str(backup_stamp)}/"
+    backup_ftp_dir = f"{HOSTNAME}-reoback"
+
+    for item_arch in config_data['backup']:
+        out_file = f"{backup_dir}{item_arch['name']}.tar.gz"
+        logger.info(f"out file {out_file}")
+
+    try:
+        f = open(nagios_file, "w")
+        f.write(str(int(time.time())))
+        f.close()
+        os.remove(pid_file)
+    except IOError as e:
+        logger.exception(f"##### {e}")
 
 
 if __name__ == "__main__":
