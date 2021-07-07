dc25ab9ad0007c0ff73c7e1ae3f542910d05fd29
antonzhelyazkov <antonisimo@gmail.com>
2021-07-07 23:18:24 +0300

    pg

--- acronis-mysql/pre.sh
+++ acronis-mysql/pre.sh
@@ -2,9 +2,12 @@
 
 MY_DATABASE="postone"
 DST_DIR="/var/tmp/mysql_backup"
+DST_DIR_TABLES=$DST_DIR"/"$MY_DATABASE
 
-for TABLE in $(mysql -N -B -e "show tables from $MY_DATABASE");
-do
-    echo "Backing up $TABLE"
-    mysqldump $MY_DATABASE "$TABLE" > $DST_DIR/"$TABLE".sql
-done;
+echo $DST_DIR_TABLES
+
+#for TABLE in $(mysql -N -B -e "show tables from $MY_DATABASE");
+#do
+#    echo "Backing up $TABLE"
+#    mysqldump $MY_DATABASE "$TABLE" > $DST_DIR/"$TABLE".sql
+#done;
