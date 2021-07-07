614c42fc843e9cb4157a5871adb45b587e4f6d76
antonzhelyazkov <antonisimo@gmail.com>
2021-07-07 23:25:46 +0300

    pg

--- acronis-mysql/pre.sh
+++ acronis-mysql/pre.sh
@@ -9,7 +9,7 @@ mkdir -p "$DST_DIR_TABLES"
 for TABLE in $(mysql -N -B -e "show tables from $MY_DATABASE");
 do
     echo "Backing up $TABLE"
-    mysqldump $MY_DATABASE "$TABLE" > $DST_DIR_TABLES/"$TABLE".sql
+    mysqldump $MY_DATABASE "$TABLE" | pigz > $DST_DIR_TABLES/"$TABLE".sql.gz
 done;
 
 mysqldump -B $MY_DATABASE | pigz > $DST_DIR"/"$MY_DATABASE.sql.gz
