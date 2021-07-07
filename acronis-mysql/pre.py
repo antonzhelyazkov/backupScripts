7b46e2d9e4cbd50a663439cf62a8cc3238978354
aantonov <antonisimo@gmail.com>
2021-07-07 16:31:02 +0300

    mysql acronis

--- acronis-mysql/pre.py
+++ acronis-mysql/pre.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import argparse
+import logging
+import os
+import sys
 
 LOG_FILE = "/var/log/mysql_backup.log"
 DEF_EXTRA_FILE = "/root/my.cnf"
+DIR_TO_BACKUP = "/var/tmp/mysql_backup"
+
+
+def add_slash(directory):
+    if not directory.endswith("/"):
+        dir_return = directory + "/"
+    else:
+        dir_return = directory
+    return dir_return
 
 
 def main():
@@ -13,8 +25,27 @@ def main():
     args_cmd = parser.parse_args()
     database = args_cmd.database
 
-    with open(LOG_FILE, "a") as log_file:
-        log_file.write(database)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    if not os.path.isdir(DIR_TO_BACKUP):
+        try:
+            os.makedirs(DIR_TO_BACKUP)
+            logger.info(f"INFO directory {DIR_TO_BACKUP} created")
+        except OSError as e:
+            logger.info(f"ERROR could not create directory {e}")
+            sys.exit(1)
+    else:
+        logger.info(f"INFO directory exists {DIR_TO_BACKUP}")
+
+    logger.info(database)
 
 
 if __name__ == "__main__":
