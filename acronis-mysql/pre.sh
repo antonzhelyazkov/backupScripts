f388ed32c5df90f50f14ef409128966f15789f5f
antonzhelyazkov <antonisimo@gmail.com>
2021-07-07 23:36:40 +0300

    pg

--- acronis-mysql/pre.sh
+++ acronis-mysql/pre.sh
@@ -3,12 +3,14 @@
 MY_DATABASE="postone"
 DST_DIR="/var/tmp/mysql_backup"
 DST_DIR_TABLES=$DST_DIR"/"$MY_DATABASE
+LOG_FILE="/var/log/acronis_pre.log"
 
 mkdir -p "$DST_DIR_TABLES"
 
+
 for TABLE in $(mysql -N -B -e "show tables from $MY_DATABASE");
 do
-    echo "Backing up $TABLE"
+    echo $(date +'%Y/%m/%d %H:%M:%S%s') "Backing up $TABLE" >> $LOG_FILE
     mysqldump $MY_DATABASE "$TABLE" | pigz > $DST_DIR_TABLES/"$TABLE".sql.gz
 done;
 
